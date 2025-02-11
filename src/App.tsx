83f09ecd7e5f55061fc983fa85f72829f42fe9ad
bowber <46023302+bowber@users.noreply.github.com>
2025-02-11 15:17:03 +0700

    feat: integrate GSAP for scoring animation and add score boxes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,54 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { createWorld } from './utils/game'
 
+const scoreBoxesInfo = [
+  { value: "29x", color: "#EA0235" },
+  { value: "4x", color: "#F52827" },
+  { value: "1.5x", color: "#F0551E" },
+  { value: "0.3x", color: "#F28412" },
+  { value: "0.2x", color: "#F6B601" },
+  { value: "0.3x", color: "#F28412" },
+  { value: "1.5x", color: "#F0551E" },
+  { value: "4x", color: "#F52827" },
+  { value: "29x", color: "#EA0235" },
+]
+
 function App() {
   const container = useRef<HTMLDivElement>(null)
+  const scoreBoxes = useRef<Array<HTMLDivElement | null>>([])
+  const [scoreBoxHistory, setScoreBoxHistory] = useState<Array<{ value: string, color: string }>>([])
   useEffect(() => {
     if (container.current === null) return;
-    const world = createWorld(container.current);
+    if (scoreBoxes.current.length === 0) return;
+    if (scoreBoxes.current.some(scoreBox => scoreBox === null)) return;
+    const world = createWorld(container.current, scoreBoxes.current as HTMLDivElement[]);
     return world.cleanup;
   }, []);
   return (
     <>
-      <div ref={container}></div>
+      <div ref={container} style={{ position: "relative" }}>
+        {scoreBoxesInfo.map((scoreBox, index) => (
+          <div
+            key={index}
+            ref={el => { scoreBoxes.current[index] = el }}
+            // Add animate-scoring class to animate the scoreBox
+            // className='animate-scoring' 
+            style={{
+              backgroundColor: scoreBox.color,
+              color: 'black',
+              position: 'absolute',
+              borderRadius: '3px',
+              borderBottom: '2px solid rgba(0, 0, 0, 0.5)',
+              width: '45px',
+              fontSize: '10px',
+              fontWeight: 'bold',
+              padding: '2px',
+            }}
+          >
+            {scoreBox.value}
+          </div>
+        ))}
+      </div>
     </>
   )
 }
--- src/utils/game.ts
+++ src/utils/game.ts
@@ -6,13 +6,19 @@ import {
   Bodies,
   Body,
   Events,
+  IEventCollision,
 } from "matter-js";
+import gsap from "gsap";
 
-const WHITE_DOTS_GROUP = 0b0001;
-const RED_DOTS_GROUP = 0b0010;
-const GROUND_GROUP = 0b0100;
+const WHITE_DOTS_CATEGORY = 0b0001;
+const RED_DOTS_CATEGORY = 0b0010;
+const GROUND_CATEGORY = 0b0100;
+const SCOREBOX_CATEGORY = 0b1000;
 
-export const createWorld = (container: HTMLDivElement) => {
+export const createWorld = (
+  container: HTMLDivElement,
+  scoreBoxElems: HTMLDivElement[]
+) => {
   // create an engine
   const engine = Engine.create();
 
@@ -23,23 +29,6 @@ export const createWorld = (container: HTMLDivElement) => {
     options: {
       wireframes: false,
     },
-  });
-  const redDotsPosition = Array.from({ length: 20 }, (_, i) => ({
-    //
-    x: 375,
-    // y from -400 - 0, spaced by 50
-    y: -400 + 400 * Math.sin((i * Math.PI) / 5),
-  }));
-
-  const redDots = redDotsPosition.map((position) => {
-    return Bodies.circle(position.x, position.y, 10, {
-      render: { fillStyle: "red" },
-      restitution: 0.8,
-      collisionFilter: {
-        category: RED_DOTS_GROUP,
-        mask: WHITE_DOTS_GROUP | GROUND_GROUP,
-      },
-    });
   });
 
   //   White dots make a triangle
@@ -54,44 +43,128 @@ export const createWorld = (container: HTMLDivElement) => {
       });
     }
   }
-  const whiteDots = whiteDotsPosition.map((position) => {
+  const whiteDots = whiteDotsPosition.map((position, index) => {
     return Bodies.circle(position.x, position.y, 5, {
       isStatic: true,
       render: { fillStyle: "white" },
+      label: "whiteDot_" + index,
       collisionFilter: {
-        category: WHITE_DOTS_GROUP,
+        category: WHITE_DOTS_CATEGORY,
       },
+    });
+  });
+  const whiteDotVFX = whiteDotsPosition.map((position) => {
+    return Bodies.circle(position.x, position.y, 10, {
+      isStatic: true,
+      isSensor: true,
+      render: { fillStyle: "white", opacity: 0 },
     });
   });
   const ground = Bodies.rectangle(400, 510, 810, 60, {
     isStatic: true,
     collisionFilter: {
-      category: GROUND_GROUP,
+      category: GROUND_CATEGORY,
     },
   });
-  const leftBorder = Bodies.rectangle(210, 300, 10, 600, {
+  const leftBorder = Bodies.rectangle(185, 370, 10, 600, {
     isStatic: true,
-    // render: { fillStyle: "transparent" },
+    render: { fillStyle: "transparent" },
   });
-  const rightBorder = Bodies.rectangle(540, 300, 10, 600, {
+  const rightBorder = Bodies.rectangle(565, 370, 10, 600, {
     isStatic: true,
-    // render: { fillStyle: "transparent" },
+    render: { fillStyle: "transparent" },
   });
   Body.rotate(leftBorder, Math.PI / 6.8);
   Body.rotate(rightBorder, -Math.PI / 6.8);
 
+  const scoreBoxes = Array.from({ length: 9 }, (_, i) => ({
+    x: 170 + 51 * i,
+    y: 470,
+    id: `scoreBox_${i}`,
+  }));
+  const scoreBoxBodies = scoreBoxes.map((box) => {
+    return Bodies.rectangle(box.x, box.y, 45, 20, {
+      isStatic: true,
+      render: { fillStyle: "transparent" },
+      label: box.id,
+      collisionFilter: {
+        category: SCOREBOX_CATEGORY,
+      },
+    });
+  });
+  // Setup the scoreBoxElems position
+  scoreBoxBodies.forEach((scoreBox, index) => {
+    const scoreBoxElem = scoreBoxElems[index];
+    if (!scoreBoxElem) return;
+    const { x, y } = scoreBox.position;
+    scoreBoxElem.style.left = `${x - 45 / 2}px`;
+    scoreBoxElem.style.top = `${y - 20 / 2}px`;
+  });
+
   //   Handle Collision
+  const handleReddotCollision = (
+    event: IEventCollision<Engine>,
+    redDotBody: Body,
+    otherBody: Body
+  ) => {
+    // Remove reddot on collide with GROUND_CATEGORY
+    if (otherBody.collisionFilter.category === GROUND_CATEGORY) {
+      Composite.remove(engine.world, redDotBody);
+    }
+    // Blink the white dot VFX on collide with WHITE_DOTS_CATEGORY
+    if (otherBody.collisionFilter.category === WHITE_DOTS_CATEGORY) {
+      const index = parseInt(otherBody.label.split("_")[1]);
+      const whiteDotVFXBody = whiteDotVFX[index];
+      if (!whiteDotVFXBody) return;
+      whiteDotVFXBody.circleRadius = 0;
+      gsap.to(whiteDotVFXBody, {
+        circleRadius: 12,
+        duration: 0.2,
+      });
+      gsap.to(whiteDotVFXBody.render, {
+        opacity: 0.7,
+        scale: 1.5,
+        duration: 0.3,
+        onComplete: () => {
+          gsap.to(whiteDotVFXBody.render, {
+            opacity: 0,
+            duration: 0.3,
+          });
+        },
+      });
+    }
+    // Animate the scoreBox on collide with scoreBox & remove the redDot
+    if (otherBody.collisionFilter.category === SCOREBOX_CATEGORY) {
+      const scoreBoxIndex = parseInt(otherBody.label.split("_")[1]);
+      const scoreBoxElem = scoreBoxElems[scoreBoxIndex];
+      if (!scoreBoxElem) return;
+      scoreBoxElem.classList.remove("animate-scoring");
+      setTimeout(() => {
+        scoreBoxElem.classList.add("animate-scoring");
+      }, 100);
+      Composite.remove(engine.world, redDotBody);
+    }
+  };
+
   Events.on(engine, "collisionStart", (event) => {
     const pairs = event.pairs;
-    console.log(pairs[0]);
+    pairs.forEach(({ bodyA, bodyB }) => {
+      if (bodyA.collisionFilter.category === RED_DOTS_CATEGORY) {
+        handleReddotCollision(event, bodyA, bodyB);
+      }
+      if (bodyB.collisionFilter.category === RED_DOTS_CATEGORY) {
+        handleReddotCollision(event, bodyB, bodyA);
+      }
+    });
   });
   // add all of the bodies to the world
   Composite.add(engine.world, [
-    ...redDots,
     ...whiteDots,
+    ...whiteDotVFX,
     ground,
     leftBorder,
     rightBorder,
+    ...scoreBoxBodies,
   ]);
 
   // run the renderer
@@ -114,8 +187,8 @@ export const createWorld = (container: HTMLDivElement) => {
       render: { fillStyle: "red" },
       restitution: 0.8,
       collisionFilter: {
-        category: RED_DOTS_GROUP,
-        mask: WHITE_DOTS_GROUP | GROUND_GROUP,
+        category: RED_DOTS_CATEGORY,
+        mask: WHITE_DOTS_CATEGORY | GROUND_CATEGORY | SCOREBOX_CATEGORY,
       },
     });
     Composite.add(engine.world, redDot);
